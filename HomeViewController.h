e990a17b38a35fff6c0e99b6ed63cb3596ea88a1
Mizunotai <mizutai731@gmail.com>
2015-01-29 22:12:51 +0900

    スタートボタンを沈むボタンにした

---
+++ HomeViewController.h
@@ -0,0 +1,13 @@
+//
+//  HomeViewController.h
+//  new taikis app
+//
+//  Created by 大氣 on 2015/01/29.
+//  Copyright (c) 2015年 Taiki. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "BButton.h"
+@interface HomeViewController : ViewController
+
+@end
--- new taikis app/AppDelegate.h
+++ new taikis app/AppDelegate.h
@@ -6,6 +6,7 @@
 //  Copyright (c) 2015年 Taiki. All rights reserved.
 //
 
+
 #import <UIKit/UIKit.h>
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
